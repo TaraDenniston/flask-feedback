0c66b40004ce53694e3e73cf82bd3f251f0002f7
Tara Denniston <taradenniston@gmail.com>
2023-03-08 17:00:05 -0500

    configure app.py and add route for /

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, redirect, render_template
+from keys import SECRET_KEY
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
+
+app = Flask(__name__)
+app.app_context().push()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///feedback'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = SECRET_KEY
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+
+@app.route('/')
+def redirect_reg():
+    return redirect('/register')
