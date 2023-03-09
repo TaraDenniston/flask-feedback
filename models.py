737f82b3575609329a21441d8eb3f3eab4148686
Tara Denniston <taradenniston@gmail.com>
2023-03-09 11:50:57 -0500

    add register() and authenticate() class methods to User class

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 def connect_db(app):
     db.app = app
@@ -22,3 +24,30 @@ class User(db.Model):
         return f'< User {self.username}: {self.first_name} {self.last_name}, {self.email} >'
 
 
+    @classmethod
+    def register(cls, username, pwd, email, first_name, last_name):
+        """Register user with hashed password and return user instance"""
+
+        # create hashed version of password to store
+        pw_hash = bcrypt.generate_password_hash(pwd).decode('utf8')
+
+        user = cls(username=username, password=pw_hash, email=email, \
+                   first_name=first_name, last_name=last_name)
+        
+        db.session.add(user)
+        db.session.commit()
+        
+        return user
+    
+    @classmethod
+    def authenticate(cls, username, pwd):
+        """Validate that user exists & password is correct
+		   Return user if valid; else return False"""
+	
+        u = User.query.filter_by(username=username).first()
+
+        if u and bcrypt.check_password_hash(u.password, pwd):
+			# return user instance
+            return u
+        else:
+            return False
