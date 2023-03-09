a9699c70d4b391a35d0f672d9e917733518f6d12
Tara Denniston <taradenniston@gmail.com>
2023-03-09 17:40:49 -0500

    create registration page

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect, render_template
 from keys import SECRET_KEY
 from flask_debugtoolbar import DebugToolbarExtension
+from forms import RegisterForm, LoginForm
 from models import db, connect_db, User
 
 app = Flask(__name__)
@@ -12,9 +13,31 @@ app.config['SECRET_KEY'] = SECRET_KEY
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
-
 connect_db(app)
 
 @app.route('/')
 def redirect_reg():
     return redirect('/register')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_user():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        # Use form data to register user
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+
+        new_user = User.register(username, password, email, first_name, last_name)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect('/secret')
+
+    return render_template('register.html', form=form)
+
+    
